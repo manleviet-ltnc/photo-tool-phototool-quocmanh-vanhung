30c4bc1a0a479c9e133c91e79912f8a95945f6e5
Nguyễn Quốc Mạnh <31847950+manh24866842@users.noreply.github.com>
2017-12-07 16:52:13 +0700

    code tool 8

---
+++ ExplorerForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Manning.MyPhotoAlbum;
+using Manning.MyPhotoControls;
+
+namespace MyAlbumExplorer
+{
+    public partial class ExplorerForm : Form
+    {
+        public ExplorerForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // Assign title bar
+            Version v = new Version(Application.ProductVersion);
+            this.Text = String.Format("MyAlbumExplorer {0:#}.{1:#}",
+                                       v.Major, v.Minor);
+
+            atvAlbumTree.Nodes.Clear();
+            atvAlbumTree.AddAlbumDirectory("Default Albums",
+                AlbumManager.DefaultPath);
+
+
+            base.OnLoad(e);
+        }
+    }
+}
---
+++ TextForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollablePictureBox_Text
+{
+    public partial class TextForm : Form
+    {
+        public TextForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                spbxImage.Image = Image.FromFile(dlg.FileName);
+            }
+        }
+
+        private void cmbSizeMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSizeMode.Text.Length > 0)
+            {
+                spbxImage.SizeMode = (PictureBoxSizeMode)
+                    Enum.Parse(typeof(PictureBoxSizeMode),
+                               cmbSizeMode.Text);
+            }
+        }
+    }
+}
